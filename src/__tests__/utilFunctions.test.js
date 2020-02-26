3d0a07743d0fcca56d9c7a96526bd4615fa24cba
Preston Jenkins <prestonjenkins@Prestons-MacBook-Pro.local>
2020-02-26 13:52:19 -0700

    just begun

---
+++ src/__tests__/utilFunctions.test.js
@@ -0,0 +1,7 @@
+import { shortenText } from '../utils/functions'
+import { wordCount, attachUserName } from '../../server/utils'
+import { shortText, longText, posts, users } from './__data__/testData'
+
+test(`Don't alter text under 100 characters`, () = {
+  
+})
